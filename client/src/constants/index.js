9d3724a8f34139861c0268997f90438d06a850e5
Sumit3972 <124435686+Sumit3972@users.noreply.github.com>
2024-06-13 17:23:55 +0530

    Add files via upload

---
+++ client/src/constants/index.js
@@ -0,0 +1,16 @@
+import {
+  dashboardIcon,
+  profileIcon,
+  transactionsIcon,
+  transferIcon,
+} from '../assets';
+
+export const navLinks = [
+  {label: 'Dashboard', icon: dashboardIcon, link: '/dashboard'},
+  {label: 'Transfer Fund', icon: transferIcon, link: '/transfer'},
+  {label: 'User Profile', icon: profileIcon, link: '/profile'},
+  {label: 'Transaction History', icon: transactionsIcon, link: '/transactions'},
+];
+
+export const soureCodeLink =
+  'https://github.com/Sumit3972';
